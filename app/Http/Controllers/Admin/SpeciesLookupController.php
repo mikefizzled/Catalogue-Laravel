b613bb1d324288743d3783996e9a557a464ce7c2
Mike Reaney <tinytange@hotmail.com>
2026-09-02 17:17:35 +0100

    Split the EBirdTaxonomy Controller into admin and public functionality
    Create a new controller for the bocc lockup and ebird data check.
    Rename old controller to better reflect the taxonomy functionality

--- app/Http/Controllers/Admin/SpeciesLookupController.php
+++ app/Http/Controllers/Admin/SpeciesLookupController.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use App\Models\ConservationList;
-use App\Models\Order;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
-class EBirdTaxonomyController extends Controller
+class SpeciesLookupController extends Controller
 {
     public function search(Request $request)
     {
@@ -89,81 +87,5 @@ class EBirdTaxonomyController extends Controller
         }
 
         return response()->json($defaultBirdData);
-    }
-
-    public function taxonomyJsonWithoutGenera()
-    {
-        $taxonomy = Order::with('families.genera.animals')->get();
-
-        $jsonStructure = [
-            'name' => 'Aves',
-            'details' => 'Birds',
-            'children' => $taxonomy->map(function ($order) {
-                return [
-                    'name' => $order->order_name,
-                    'children' => $order->families->map(function ($family) {
-                        return [
-                            'name' => $family->family_name,
-                            'details' => $family->common_name,
-                            'children' => $family->genera->flatMap(function ($genus) {
-                                return $genus->animals->map(function ($animal) {
-                                    return [
-                                        'name' => $animal->common_name,
-                                        'url' => '/birds/'.$animal->slug,
-                                        'image' => Storage::disk('s3')->url('thumbnails/'.$animal->thumbnail_url),
-                                        'details' => $animal->scientific_name,
-                                    ];
-                                });
-                            })->toArray(),
-                        ];
-                    })->toArray(),
-                ];
-            })->toArray(),
-        ];
-
-        return response()->json($jsonStructure);
-    }
-
-    public function conservation()
-    {
-        $conservationLists = ConservationList::get();
-
-        return view('conservation', ['conservationLists' => $conservationLists]);
-    }
-
-    public function taxonomyJsonWithGenera()
-    {
-        $taxonomy = Order::with('families.genera.animals')->get();
-
-        $jsonStructure = [
-            'name' => 'Aves',
-            'details' => 'Birds',
-            'children' => $taxonomy->map(function ($order) {
-                return [
-                    'name' => $order->order_name,
-                    'children' => $order->families->map(function ($family) {
-                        return [
-                            'name' => $family->family_name,
-                            'details' => $family->common_name,
-                            'children' => $family->genera->map(function ($genus) {
-                                return [
-                                    'name' => $genus->genus_name,
-                                    'children' => $genus->animals->map(function ($animal) {
-                                        return [
-                                            'name' => $animal->common_name,
-                                            'url' => '/birds/'.$animal->slug,
-                                            'image' => Storage::disk('s3')->url('thumbnails/'.$animal->thumbnail_url),
-                                            'details' => $animal->scientific_name,
-                                        ];
-                                    })->toArray(),
-                                ];
-                            })->toArray(),
-                        ];
-                    })->toArray(),
-                ];
-            })->toArray(),
-        ];
-
-        return response()->json($jsonStructure);
     }
 }
